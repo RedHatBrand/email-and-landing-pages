2aee3dc07d9e07e87415aa0cf763f43cbec02710
John Morris <johnmorris1095@gmail.com>
2015-04-08 09:41:10 +1000

    Update 2015-04-07T23:41:03.819Z

---
+++ assets/javascripts/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+//  $(document).on("scroll", function () {
+//    var scrollPos = $(document).scrollTop();
+//    var heroOffset = scrollPos * (1/-2);
+//    $(".hero").css("background-position-y",heroOffset);
+//  });
+//  
+  $("#call-to-action").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#form-section").offset().top
+    }, 1000);
+  });
+});
