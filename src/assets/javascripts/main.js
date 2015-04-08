530ff67366d5afb70172286aac15b59df48973f2
John Morris <johnmorris1095@gmail.com>
2015-04-08 11:13:21 +1000

    content mirroring

--- src/assets/javascripts/main.js
+++ src/assets/javascripts/main.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
-  $(document).on("scroll", function () {
-    var scrollPos = $(document).scrollTop();
-    var heroOffset = scrollPos * (1/-2);
-    $(".hero").css("background-position-y",heroOffset);
-  });
-  
+
+//  $(document).on("scroll", function () {
+//    var scrollPos = $(document).scrollTop();
+//    var heroOffset = scrollPos * (1/-2);
+//    $(".hero").css("background-position-y",heroOffset);
+//  });
+//  
   $("#call-to-action").click(function() {
     $('html, body').animate({
         scrollTop: $("#form-section").offset().top
